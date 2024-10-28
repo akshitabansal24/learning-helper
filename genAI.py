34b196bfda07093028fe41841c4cf1fddbd2284d
akshitabansal <akshitabansal24@gmail.com>
2024-10-28 10:11:03 +0000

    speechtotext added projId changed

--- genAI.py
+++ genAI.py
@@ -1,27 +1,7 @@
-# import google.generativeai as genai
-# import vertexai
-# from vertexai.generative_models import GenerativeModel, ChatSession
-
-# model = genai.GenerativeModel('gemini-1.5-flash')
-
-# project_id = "learning-helper-2024"
-# vertexai.init(project=project_id, location="us-central1")
-
-# def get_chat_response(chat: ChatSession, prompt: str) -> str:
-#     text_response = []
-#     responses = chat.send_message(prompt, stream=True)
-#     for chunk in responses:
-#         text_response.append(chunk.text)
-#     return "".join(text_response)
-
-# chat = model.start_chat(history=[])
-
-# print(get_chat_response(chat, 'Hi'))
-
 import vertexai
 from vertexai.generative_models import GenerativeModel
 
-PROJECT_ID = "learning-helper-2024"
+PROJECT_ID = "ambient-union-440009-q4"
 vertexai.init(project=PROJECT_ID, location="us-central1")
 
 model = GenerativeModel("gemini-1.5-flash-002")
