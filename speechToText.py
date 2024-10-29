bab076f485fdbfaa8f80cfe0b0b6d6be843b9e7c
akshitabansal <akshitabansal24@gmail.com>
2024-10-29 17:29:52 +0000

    speechTotextFixed

--- speechToText.py
+++ speechToText.py
@@ -1,30 +1,29 @@
-
-# Imports the Google Cloud client library
-
-
 from google.cloud import speech
-
-
 
 def run_quickstart() -> speech.RecognizeResponse:
     # Instantiates a client
     client = speech.SpeechClient()
 
     # The name of the audio file to transcribe
-    gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
-
-    audio = speech.RecognitionAudio(uri=gcs_uri)
+    # gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
+    file_path = "/home/learninghelper2/learning-helper/output.mp3"
+    with open(file_path, "rb") as audio_file:
+        content = audio_file.read()
+    
+    audio = speech.RecognitionAudio(content=content)
 
     config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=16000,
+        encoding=speech.RecognitionConfig.AudioEncoding.MP3,
         language_code="en-US",
     )
 
     # Detects speech in the audio file
     response = client.recognize(config=config, audio=audio)
-
+    
+    str=""
     for result in response.results:
-        print(f"Transcript: {result.alternatives[0].transcript}")
+        str=str+result.alternatives[0].transcript+'.'
+
+    print(str)
 
 run_quickstart()
